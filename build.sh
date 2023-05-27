97873a37c392bc9ddabfec2928a97c3e0713d9a3
Lucas Zurverra <lzurverra@gmail.com>
2023-05-27 19:06:51 -0300

    build pylibdmtx

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install libportaudio2 -y
 sudo apt-get install libasound-dev -y
+pip install pylibdmtx
+pip install pylibdmtx[scripts]
 pip install -r requirements.txt
