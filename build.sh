4bf019c1a81c35014b94b45107c956ecee986a14
Lucas Zurverra <lzurverra@gmail.com>
2023-05-27 18:23:54 -0300

    added superuser permission for build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install libportaudio2
-sudo apt-get install libasound-dev
+sudo apt-get install libportaudio2 -y
+sudo apt-get install libasound-dev -y
 pip install -r requirements.txt
