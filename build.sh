c6600ca33512b31a0935849bd286029d6e580023
Lucas Zurverra <lzurverra@gmail.com>
2023-05-31 12:50:41 -0300

    unabled mic feature

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sudo apt-get install libportaudio2 -y
-sudo apt-get install libasound-dev -y
-pip install --upgrade pip setuptools wheel
-pip install p5py
-pip install PEP517
+#sudo apt-get install libportaudio2 -y
+#sudo apt-get install libasound-dev -y
+#pip install --upgrade pip setuptools wheel
+#pip install p5py
+#pip install PEP517
 pip install pylibdmtx
 pip install pylibdmtx[scripts]
-apt install build-essential portaudio19-dev
+#apt install build-essential portaudio19-dev
 pip install -r requirements.txt
